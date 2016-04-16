01fe4c852ad23990a2a3c94602546c7c03ea90fe
biao <iambillzhao@gmail.com>
2016-04-16 20:43:13 +0800

    update for migrating to es6

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,20 @@ require('../css/style.css');
 require('../css/swiper.css');
 
 
-var CURRENT_ENV = 'dev';
+const CURRENT_ENV = 'dev';
 
 
 // basic global variables for the whole app
-var React = require('react'),
-    ReactDOM = require('react-dom'),
-    $ = require('jquery'),
-    Swiper = require('swiper');
+const React = require('react'),
+      ReactDOM = require('react-dom'),
+      $ = require('jquery'),
+      Swiper = require('swiper');
 
 // got the windowHegith at the very beginning in order to prevent the visual keyboard impact this value
-var windowHeight = $(window).height();
+const windowHeight = $(window).height();
 
 
-var url = CURRENT_ENV === 'dev' ? {
+const url = CURRENT_ENV === 'dev' ? {
                 keywordsURL: '../../data/keywords.json',
                 defaultContentURL: '../../data/defaultresults.json',
                 searchURL: '../../data/keywordresults.json'
@@ -26,108 +26,112 @@ var url = CURRENT_ENV === 'dev' ? {
             }
 
 
-var TopBar = React.createClass({
-    render: function() {
-        return (
-            <div className="top-bar">
-                <div className="search-icon"></div>
-                <SearchBar value={this.props.curKeyword} searchContentUpdate={this.props.onSearchContentUpdated}/>
-                <span className="delete-btn" onClick={this.props.onDeleteContent}></span>
-            </div>
-        );
-    }
-});
+const TopBar = ({curKeyword, onDeleteContent, onSearchContentUpdated}) => (
+    <div className="top-bar">
+        <div className="search-icon"></div>
+        <SearchBar value={curKeyword} searchContentUpdate={onSearchContentUpdated}/>
+        <span className="delete-btn" onClick={onDeleteContent}></span>
+    </div>
+);
 
-var SearchBar = React.createClass({
-    render: function() {
-        return (
-            <input type="text" placeholder="请输入你想搜索的内容" className="search-bar" value={this.props.value}
-            onChange={this.props.searchContentUpdate}
-            onKeyDown={this.props.searchContentUpdate}
-            onBlur={this.props.searchContentUpdate}
-            />
-        );
-    }
-});
 
-var KeywordsBar = React.createClass({
-    _initSwiper: function() {
+const SearchBar = ({value, searchContentUpdate}) => (
+    <input type="text" placeholder="请输入你想搜索的内容" className="search-bar" value={value}
+        onChange={searchContentUpdate}
+        onKeyDown={searchContentUpdate}
+        onBlur={searchContentUpdate}
+    />
+);
+
+class KeywordsBar extends React.Component {
+    _initSwiper() {
         new Swiper(ReactDOM.findDOMNode(this), {
             slidesPerView: 'auto',
             grabCursor: true,
             slideElement: 'li',
             watchSlidesVisibility: true
         })
-    },
-    keywordClicked: function(i) {
+    }
+
+    keywordClicked(i) {
         this.props.onKeywordSelected(i.name);
-    },
-    componentDidUpdate: function() {
+    }
+
+    componentDidUpdate() {
         this._initSwiper();
-    },
-    render: function() {
+    }
+
+    render() {
         var thisComponent = this;
+
         return (
             <div className="swiper-container keywords-bar">
                 <ul className="swiper-wrapper">
                     {this.props.keywords.map(function(keyword, index) {
-                        return <li className="swiper-slide" key={index}  onClick={thisComponent.keywordClicked.bind(null, keyword)}>{keyword.name}</li>
+                        return <li
+                                className="swiper-slide"
+                                key={index}
+                                onClick={thisComponent.keywordClicked.bind(thisComponent, keyword)}
+                               >
+                                {keyword.name}
+                            </li>
                     })}
                 </ul>
             </div>
         );
     }
-});
+}
 
 
-var ResultItem = React.createClass({
-    render: function() {
-        return (
-            <li className="result-item" >
-                <a href={this.props.contentUrl}>
-                    <img src={this.props.thumbnail} />
-                    <div className="flex-box">
-                        <div dangerouslySetInnerHTML={{__html: this.props.content}}></div>
-                    </div>
-                </a>
-            </li>
-        );
-    }
-});
-
-var ResultList = React.createClass({
-    render: function() {
-        return (
-            <ul>
-                {this.props.results.map(function(result, index) {
-                    return <ResultItem key={index} contentUrl={result.url} thumbnail={result.pic} content={result.title}/>
-                })}
-            </ul>
-        );
-    }
-});
-
-var EmptyMsg = React.createClass({
-    render: function() {
-        return (
-            <div className="empty-result">
-                <p>我们暂时还未发布相关内容哦，你可以将关键词</p>
-                <p>发送给我们，我会督促小编尽快撰写~</p>
+const ResultItem = ({contentUrl, thumbnail, content}) => (
+    <li className="result-item" >
+        <a href={contentUrl}>
+            <img src={thumbnail} />
+            <div className="flex-box">
+                <div dangerouslySetInnerHTML={{__html: content}}></div>
             </div>
-        );
-    }
-});
+        </a>
+    </li>
+);
 
-var SearchApp = React.createClass({
-    _setScrollAreaHeight: function() {
+
+
+const ResultList = ({results}) => (
+    <ul>
+        {results.map(function(result, index) {
+            return <ResultItem key={index} contentUrl={result.url} thumbnail={result.pic} content={result.title}/>
+        })}
+    </ul>
+);
+
+const EmptyMsg = () => (
+    <div className="empty-result">
+        <p>我们暂时还未发布相关内容哦，你可以将关键词</p>
+        <p>发送给我们，我会督促小编尽快撰写~</p>
+    </div>
+);
+
+class SearchApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+                curKeyword: '',
+                keywords: [],
+                isDefault: true,
+                resultList: []
+        };
+    }
+
+    _setScrollAreaHeight() {
         var $scrollArea = $('.scroll-area');
         var wh = windowHeight;
         var th = $('.top-area').height();
 
         $scrollArea.css('height', wh - th + 'px');
         $scrollArea.scrollTop(0);
-    },
-    _getKeywordsList: function() {
+    }
+
+    _getKeywordsList() {
         $.ajax({
             url: url.keywordsURL,
             success: function(data) {
@@ -136,8 +140,9 @@ var SearchApp = React.createClass({
                 });
             }.bind(this)
         });
-    },
-    _getDefaultContent: function() {
+    }
+
+    _getDefaultContent() {
         $.ajax({
             url: url.defaultContentURL,
             success: function(data) {
@@ -147,8 +152,9 @@ var SearchApp = React.createClass({
                 }, this._setScrollAreaHeight);
             }.bind(this)
         });
-    },
-    _getContentWithInputValue: function() {
+    }
+
+    _getContentWithInputValue() {
         $.ajax({
             url: url.searchURL + (CURRENT_ENV ==='dev' ? '' : this.state.curKeyword),
             success: function(data) {
@@ -158,25 +164,20 @@ var SearchApp = React.createClass({
                 }, this._setScrollAreaHeight);
             }.bind(this)
         });
-    },
-    updateWithKeyword: function(keywordBeSelected) {
+    }
+
+    updateWithKeyword(keywordBeSelected) {
         this.setState({
             curKeyword: keywordBeSelected
         }, this._getContentWithInputValue);
-    },
-    getInitialState: function() {
-        return {
-                curKeyword: '',
-                keywords: [],
-                isDefault: true,
-                resultList: []
-        };
-    },
-    componentDidMount: function() {
+    }
+
+    componentDidMount() {
        this._getKeywordsList();
        this._getDefaultContent();
-    },
-    updateWithSearchContent: function(e) {
+    }
+
+    updateWithSearchContent(e) {
         if (e.keyCode === 13 || e.type === 'blur') {
             this._getContentWithInputValue();
         } else {
@@ -184,26 +185,29 @@ var SearchApp = React.createClass({
                 curKeyword: e.target.value
             })
         }
-    },
-    deleteInputContent: function() {
+    }
+
+    deleteInputContent() {
         this.setState({
             curKeyword: ''
         }, this._getDefaultContent);
-    },
-    componentDidUpdate: function() {
+    }
+
+    componentDidUpdate() {
         this._setScrollAreaHeight();
-    },
-    render: function() {
+    }
+
+    render() {
         return (
             <div className="search-view">
                 <div className="top-area">
                     <TopBar curKeyword={this.state.curKeyword}
-                        onSearchContentUpdated={this.updateWithSearchContent}
-                        onDeleteContent={this.deleteInputContent}
+                        onSearchContentUpdated={this.updateWithSearchContent.bind(this)}
+                        onDeleteContent={this.deleteInputContent.bind(this)}
                     />
                     <KeywordsBar
                         keywords={this.state.keywords}
-                        onKeywordSelected={this.updateWithKeyword}
+                        onKeywordSelected={this.updateWithKeyword.bind(this)}
                     />
                 </div>
                 <div className="scroll-area">
@@ -214,6 +218,6 @@ var SearchApp = React.createClass({
             </div>
         );
     }
-});
+}
 
 ReactDOM.render(<SearchApp />, document.getElementById('search-app'));
